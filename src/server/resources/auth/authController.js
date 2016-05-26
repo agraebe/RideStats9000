a32815d56b193f649f321ac163052f6a38c27b27
Zachary Lester <zachary.greg.lester@gmail.com>
2016-05-25 18:12:02 -0700

    Modify authController

--- src/server/resources/auth/authController.js
+++ src/server/resources/auth/authController.js
@@ -1,16 +1,15 @@
-var uber = require('../uber/uberClient.js');
+const uber = require('../uber/uberClient.js');
 
-var retrieveAuthorizedUrl = (req, res) => {
-  var url = uber.getAuthorizeUrl(['history', 'profile', 'request', 'places']);
+const retrieveAuthorizedUrl = (req, res) => {
+  const url = uber.getAuthorizeUrl(['history', 'profile', 'request', 'places']);
   res.redirect(url);
 }
 
-var handleAuthCallback = (req, res) => {
-  uber.authorization({
-    authorization_code: req.query.code
-  }, (err, access_token, refresh_token) => {
+const handleAuthCallback = (req, res) => {
+  const authorization_code = req.query.code;
+  uber.authorization({ authorization_code }, (err, access_token, refresh_token) => {
     if (err) {
-      return console.error(err);
+      return res.status(500).json(err);
     }
     // store the user id and associated access token
     // redirect the user back to your actual app
