01d3384d2c706a1f7e2807c316db9c6766c6dac9
Zachary Lester <zachary.greg.lester@gmail.com>
2016-06-03 09:29:47 -0700

    Fix config

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ module.exports = {
     client_id: process.env.UBER_CLIENT_ID,
     client_secret: process.env.UBER_CLIENT_SECRET,
     server_token: process.env.UBER_SERVER_TOKEN,
-    redirect_uri: proasfascess.env.UBER_REDRECT_URI,
+    redirect_uri: process.env.UBER_REDRECT_URI,
     name: 'RideEye',
     language: 'en_US',
     sandbox: true,
