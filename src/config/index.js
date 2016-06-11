0fbed4c52291ae78fac1ab6dc7030197d081f772
Zachary Lester <zachary.greg.lester@gmail.com>
2016-06-10 21:29:30 -0700

    Update config

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     client_secret: 'YOUR_CLIENT_SECRET_HERE',
     server_token: 'YOUR_SERVER_HERE',
     redirect_uri: 'YOUR_REDIRECT_URI_HERE', // http://localhost:3000/api/auth/callback if running locally
-    name: 'YOUR_CLIENT_ID_HERE',
+    name: 'YOUR_CLIENT_NAME_HERE',
     language: 'en_US',
     sandbox: true,
     scope: ['history', 'profile', 'places']
